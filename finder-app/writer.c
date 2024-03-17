5467acc4ff3cd374ac44eb198d61ebebbc12f6fb
Tommaso Labieni <259650@studenti.unimore.it>
2024-03-17 15:58:16 +0100

    Complete assignment 2

---
+++ finder-app/writer.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <syslog.h>
+
+int main(int argc, char *argv[])
+{
+    openlog(NULL, 0, LOG_USER);
+
+    if (argc != 3)
+    {
+        syslog(LOG_ERR, "Wrong number of params. Usage is: %s <outfile> <str>\n", argv[0]);
+
+        exit(1);
+    }
+
+    FILE *f = fopen(argv[1], "w");
+
+    if (f == NULL)
+    {
+        syslog(LOG_ERR, "ERROR While opening file %s\n", argv[1]);
+
+        exit(2);
+    }
+
+    if (fputs(argv[2], f) != 0)
+    {
+        syslog(LOG_ERR, "ERROR While writing string %s to file %s\n", argv[2], argv[1]);
+
+        exit(3);
+    }
+
+    syslog(LOG_DEBUG, "Writing %s to %s\n", argv[2], argv[1]);
+
+    return 0;
+}
